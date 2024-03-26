05ee0d545596787c6f94de45f7b4a883c143d56f
Nandhakumar Ranganathan Ganesh <79744669+Nandhakumar-rg@users.noreply.github.com>
2024-03-26 13:22:07 -0400

    Commit Part 1

--- part1.py
+++ part1.py
@@ -15,52 +15,65 @@ from sklearn.cluster import AgglomerativeClustering, KMeans
 import pickle
 import utils as u
 
-def generate_datasets():
-    n_samples = 100
-    random_state = 42
-    noisy_circles = datasets.make_circles(n_samples=n_samples, factor=.5, noise=.05, random_state=random_state)
-    noisy_moons = datasets.make_moons(n_samples=n_samples, noise=.05, random_state=random_state)
-    blobs = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
-    varied = datasets.make_blobs(n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5], random_state=random_state)
-    aniso = datasets.make_blobs(n_samples=n_samples, random_state=8)
-    transformation = [[0.6, -0.6], [-0.4, 0.8]]
-    aniso = (np.dot(aniso[0], transformation), aniso[1])
-    return {"nc": noisy_circles, "nm": noisy_moons, "bvv": varied, "add": aniso, "b": blobs}
-
 def fit_kmeans(data, n_clusters):
+    """
+    Fits k-means to the data and returns the labels.
+
+    :param data: Dataset to cluster.
+    :param n_clusters: Number of clusters to fit.
+    :return: Predicted labels from k-means clustering.
+    """
+    # Standardize the data
     scaler = StandardScaler()
     data_scaled = scaler.fit_transform(data)
+
+    # Fit KMeans
     kmeans = KMeans(n_clusters=n_clusters, init='random', random_state=42)
     kmeans.fit(data_scaled)
+
     return kmeans.labels_
 
 def compute():
     answers = {}
-    datasets = generate_datasets()
-    answers["1A: datasets"] = datasets
 
-    # Part B: Modify the `fit_kmeans` signature and implement the functionality
-    def fit_kmeans(data, n_clusters):
-        scaler = StandardScaler()
-        data_scaled = scaler.fit_transform(data[0])  
-        kmeans = KMeans(n_clusters=n_clusters, init='random', random_state=42)
-        kmeans.fit(data_scaled)
-        return kmeans.labels_
+    # Datasets
+    n_samples = 100
+    noisy_circles = datasets.make_circles(n_samples=n_samples, factor=0.5, noise=0.05, random_state=42)
+    noisy_moons = datasets.make_moons(n_samples=n_samples, noise=0.05, random_state=42)
+    blobs_varied = datasets.make_blobs(n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5], random_state=42)
+    random_state = 170
+    X, y = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
+    transformation = [[0.6, -0.6], [-0.4, 0.8]]
+    X_aniso = np.dot(X, transformation)
+    aniso = (X_aniso, y)
+    varied = datasets.make_blobs(n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5], random_state=42)
+
+    datasets_list = [
+        (noisy_circles, 'noisy_circles'),
+        (noisy_moons, 'noisy_moons'),
+        (blobs_varied, 'blobs_varied'),
+        (aniso, 'aniso'),
+        (varied, 'varied')
+    ]
+
+    answers["1A: datasets"] = {name: data for data, name in datasets_list}
+
+    k_values = [2, 3, 5, 10]
+    kmeans_results = {}
+    for data, name in datasets_list:
+        results = {}
+        for k in k_values:
+            labels = fit_kmeans(data[0], k)
+            results[k] = labels
+        kmeans_results[name] = (data, results)
 
     answers["1B: fit_kmeans"] = fit_kmeans
+    answers["1C: cluster successes"] = {"nc": [2], "nm": [2, 3], "b": [3, 5, 10]}
+    answers["1C: cluster failures"] = ["bvv", "add"]
+    answers["1D: datasets sensitive to initialization"] = ["nm", "bvv"] 
 
-    # Part C: Prepare for plotting
-    k_values = [2, 3, 5, 10]
-    kmeans_dct = {dataset_name: (data, {k: fit_kmeans(data, k) for k in k_values}) for dataset_name, data in datasets.items()}
-    file_nm = "cluster_plots.pdf"
-    myplt.plot_part1C(kmeans_dct, file_nm)
-
-
-    answers["1C: cluster successes"] = {"nc": [2], "nm": [2, 3], "b": [3, 5, 10]}  
-    answers["1C: cluster failures"] = ["bvv", "add"]  
-
-   
-    answers["1D: datasets sensitive to initialization"] = ["nm", "bvv"]  
+    # Generate plots
+    myplt.plot_part1C(kmeans_results, 'kmeans_clusters.pdf')
 
     return answers
 
